7bc1aa0169576e566b48da936e8b2520506b87c8
Alex Flores_escritorio <alexander65.af@gmail.com>
2025-06-26 19:47:27 -0400

    [ADD] Almacenamiento de lista de carrito en LocalStorage

--- 06-shopping-cart/src/context/cart.jsx
+++ 06-shopping-cart/src/context/cart.jsx
@@ -1,44 +1,11 @@
 import { createContext, useReducer } from "react";
+import { cartInitialState, cartReducer } from "../reducers/cart";
 
 export const CartContext = createContext()
 
-const initialState = []
-const reducer = (state, action) => {
-    const { type: actionType, payload: actionPayload } = action
-    switch (actionType) {
-        case 'ADD_TO_CART': {
-            const { id } = actionPayload
-            const productInCartIndex = state.findIndex(item => item.id === id)
-
-            if (productInCartIndex >= 0) {
-                const newState = structuredClone(state)
-                newState[productInCartIndex].quantity += 1
-                return newState
-            }
-
-            return [
-                ...state,
-                {
-                    ...actionPayload, //El producto que se agrega
-                    quantity: 1
-                }
-            ]
-        }
-        case 'REMOVE_FROM_CART': {
-            const { id } = actionPayload
-            return state.filter(item => item.id !== id)
-        }
-
-        case 'CLEAR_CART': {
-            return initialState
-        }
-    }
-    return state
-}
-
 export function CartProvider({ children }) {
     //const [cart, setCart] = useState([])
-    const [state, dispatch] = useReducer(reducer, initialState)
+    const [state, dispatch] = useReducer(cartReducer, cartInitialState)
 
     const addToCart = product => dispatch({
         type: 'ADD_TO_CART',
